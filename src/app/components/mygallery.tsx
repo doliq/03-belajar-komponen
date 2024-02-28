cc3ccb30a0fa5056bca3ddd8fe4fe127628c5d83
Diwa Oliq <diwaoliq@gmail.com>
2024-02-28 16:03:04 +0700

    W03: Jawaban soal 8

--- src/app/components/mygallery.tsx
+++ src/app/components/mygallery.tsx
@@ -1,24 +1,57 @@
-export default function MyProfileV2() {
-    return (
-      <div>
-        <div className="card">
-          <div className="card-content">
-            <h1>Foto</h1>
-            <img
-              className="avatar"
-              src="https://i.imgur.com/OKS67lhm.jpg"
-              alt="Aklilu Lemma"
-              width={70}
-              height={70}
-            />
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-content">
-            <h1>Tentang</h1>
-            <p>Aklilu Lemma adalah seorang ilmuwan terkemuka dari etiopia yang telah menemukan pengobatan alami untuk skistosomiasis.</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+import { getImageUrl } from '@/utils/utils';
+
+export default function MyGallery() {
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      <section className="profile">
+        <h2>Maria Skłodowska-Curie</h2>
+        <img
+          className="avatar"
+          src={getImageUrl('szV5sdG')}
+          alt="Maria Skłodowska-Curie"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profesi: </b> 
+            Fisikawan dan kimiawan
+          </li>
+          <li>
+            <b>Penghargaan: 4 </b> 
+            (Penghargaan Nobel Fisika, Penghargaan Nobel Kimia, Medali Davy, Medali Matteucci)
+          </li>
+          <li>
+            <b>Telah Menemukan: </b>
+            polonium (unsur kimia)
+          </li>
+        </ul>
+      </section>
+      <section className="profile">
+        <h2>Katsuko Saruhashi</h2>
+        <img
+          className="avatar"
+          src={getImageUrl('YfeOqp2')}
+          alt="Katsuko Saruhashi"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profesi: </b> 
+            Ahli Geokimia
+          </li>
+          <li>
+            <b>Penghargaan: 2 </b> 
+            (Penghargaan Miyake Geokimia, Penghargaan Tanaka)
+          </li>
+          <li>
+            <b>Telah Menemukan: </b>
+            sebuah metode untuk mengukur karbon dioksida pada air laut
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,4 +1,13 @@
-export function getImageUrlV2(person, size) {
+export function getImageUrl(imageId, size = 's') {
+    return (
+      'https://i.imgur.com/' +
+      imageId +
+      size +
+      '.jpg'
+    );
+  }
+
+  export function getImageUrlV2(person, size) {
     return (
       'https://i.imgur.com/' +
       person.imageId +
