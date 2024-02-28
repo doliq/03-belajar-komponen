62ea75146244446d408a3f4691428212ce039f69
Diwa Oliq <diwaoliq@gmail.com>
2024-02-28 15:25:05 +0700

    W03: Jawaban soal 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,31 @@ import Profile from "./components/profile";
 import "./globals.css";
 import { Gallery } from "./components/gallery";
 import Bio from "./components/bio";
+import TodoList from "./components/todolist";
 
+
+// export default function Home() {
+//   return (
+//     <main className="">
+//       <div>
+//         <center>
+//           {/* <h1>Ilmuwan Luar Biasa</h1> */}
+//         </center>
+//           {/* <Gallery/> */}
+//           <Bio/>
+//       </div>
+//   </main>
+// );
+// }
 
 export default function Home() {
   return (
-    <main className="">
-      <div>
-        <center>
-          {/* <h1>Ilmuwan Luar Biasa</h1> */}
-        </center>
-          {/* <Gallery/> */}
-          <Bio/>
-      </div>
-  </main>
-);
+    <section>
+      <h1 className="font-semibold text-slate-900 truncate pr-20 text-center">Ilmuwan yang luar biasa</h1>
+      <hr />
+      <Gallery />
+      <hr />
+      <TodoList />
+    </section>
+  );
 }
